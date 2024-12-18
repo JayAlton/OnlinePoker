04f845521613fbf887ac65b909b3f02c239df1e6
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-18 14:27:43 -0800

    backend
    started backend JS code with JWT functionality

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+var cors = require('cors');
+const http = require('http');
+const socketIO = require('socket.io');
+
+dotenv.config();
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const socketServer = socketIO(httpServer, {
+    cors: {
+        origin: '*',
+    },
+})
+
+app.use(cors({ origin: true, credentials: true}));
+
+app.use(express.json({extended:false}));
+
+app.use('/api/users', userRoutes);
